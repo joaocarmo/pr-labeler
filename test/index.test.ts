df5a9c1da9fdbffdd549541a0a8879e15be4f241
João Carmo <kingjoao@gmail.com>
2021-09-13 23:35:44 +0100

    Updated dependencies and added tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,26 +1,32 @@
 import nock from 'nock'
+import { readFileSync } from 'fs'
+import { join } from 'path'
 import { Probot, ProbotOctokit } from 'probot'
-import * as fs from 'fs'
-import * as path from 'path'
 import prLabeler from '../src'
 import { HOOK_NAME_PR } from '../src/constants'
+import prEditedPayload from './fixtures/pull_request.edited.json'
 import prOpenedPayload from './fixtures/pull_request.opened.json'
 import repoLabels from './fixtures/repo.labels.json'
 
 const route = require('nock-knock/lib').default
 
-const privateKey = fs.readFileSync(
-  path.join(__dirname, 'fixtures/mock-cert.pem'),
+// Get config files
+const privateKey = readFileSync(
+  join(__dirname, 'fixtures', 'mock-cert.pem'),
   'utf-8',
 )
-
-const configFile = fs.readFileSync(
-  path.join(__dirname, 'fixtures/mock-config.yml'),
+const configFile = readFileSync(
+  join(__dirname, 'fixtures', 'mock-config.yml'),
   'utf-8',
 )
 
 describe('PR Labeler', () => {
   let probot: any
+
+  const testCases = [
+    ['opened', prOpenedPayload, 'feature'],
+    ['edited', prEditedPayload, 'wip'],
+  ]
 
   beforeEach(() => {
     nock.disableNetConnect()
@@ -53,49 +59,61 @@ describe('PR Labeler', () => {
     nock.enableNetConnect()
   })
 
-  test('adds labels when a pr is opened (no config)', async () => {
-    const mock = nock('https://api.github.com')
-      // Test with no config
-      .get(route(`/repos/:owner/:repo/contents/:path`))
-      .reply(404)
-      .get(route(`/repos/:owner/.github/contents/:path`))
-      .reply(404)
+  test.each(testCases)(
+    'Adds no labels when a PR is %s (no config)',
+    async (action, payload) => {
+      const mock = nock('https://api.github.com')
+        // Test with no config
+        .get(route(`/repos/:owner/:repo/contents/:path`))
+        .reply(404)
+        .get(route(`/repos/:owner/.github/contents/:path`))
+        .reply(404)
 
-    // Receive a webhook event
-    await probot.receive({ name: HOOK_NAME_PR, payload: prOpenedPayload })
+      // Make sure the payload is correct
+      expect((payload as any).action).toBe(action)
 
-    expect(mock.pendingMocks()).toStrictEqual([])
-  })
+      // Receive a webhook event
+      await probot.receive({ name: HOOK_NAME_PR, payload })
 
-  test('adds labels when a pr is opened (with config)', async () => {
-    const mock = nock('https://api.github.com')
-      // Test with no config
-      .get(route(`/repos/:owner/:repo/contents/:path`))
-      .reply(200, configFile)
-      // Test that we return the correct repo labels
-      .get(route('/repos/:owner/:repo/labels'))
-      .reply(200, repoLabels)
-      // Test that we correctly create the correct labels
-      .post(route('/repos/:owner/:repo/labels'), (body: any) => {
-        expect(body.name).toBe('feature')
-        return true
-      })
-      .reply(200, {})
-      // Test that we correctly update the correct labels
-      .post(
-        route('/repos/:owner/:repo/issues/:issue_number/labels'),
-        (body: any) => {
-          expect(Array.isArray(body.labels)).toBe(true)
-          expect(body.labels.length).toBe(1)
-          expect(body.labels).toStrictEqual(['feature'])
+      expect(mock.pendingMocks()).toStrictEqual([])
+    },
+  )
+
+  test.each(testCases)(
+    'Adds one label when a PR is %s (with config)',
+    async (action, payload, label) => {
+      const mock = nock('https://api.github.com')
+        // Test with no config
+        .get(route(`/repos/:owner/:repo/contents/:path`))
+        .reply(200, configFile)
+        // Test that we return the correct repo labels
+        .get(route('/repos/:owner/:repo/labels'))
+        .reply(200, repoLabels)
+        // Test that we correctly create the correct labels
+        .post(route('/repos/:owner/:repo/labels'), (body: any) => {
+          expect(body.name).toBe(label)
           return true
-        },
-      )
-      .reply(200, {})
+        })
+        .reply(200, {})
+        // Test that we correctly update the correct labels
+        .post(
+          route('/repos/:owner/:repo/issues/:issue_number/labels'),
+          (body: any) => {
+            expect(Array.isArray(body.labels)).toBe(true)
+            expect(body.labels.length).toBe(1)
+            expect(body.labels).toStrictEqual([label])
+            return true
+          },
+        )
+        .reply(200, {})
 
-    // Receive a webhook event
-    await probot.receive({ name: HOOK_NAME_PR, payload: prOpenedPayload })
+      // Make sure the payload is correct
+      expect((payload as any).action).toBe(action)
 
-    expect(mock.pendingMocks()).toStrictEqual([])
-  })
+      // Receive a webhook event
+      await probot.receive({ name: HOOK_NAME_PR, payload })
+
+      expect(mock.pendingMocks()).toStrictEqual([])
+    },
+  )
 })
