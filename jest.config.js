0bd858deebf52fb152112366b10ca744dd4dfb85
João Carmo <kingjoao@gmail.com>
2021-11-07 12:04:23 +0000

    Ignored the any type error on tests

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,15 @@
 const esModules = ['escape-string-regexp'].join('|');
 
 module.exports = {
+  globals: {
+    'ts-jest': {
+      diagnostics: {
+        ignoreCodes: [
+          'TS7006', /* Parameter 'string' implicitly has an 'any' type. */
+        ],
+      },
+    },
+  },
   roots: ['<rootDir>/src/', '<rootDir>/test/'],
   transform: {
     '^.+\\.[tj]sx?$': 'ts-jest',
