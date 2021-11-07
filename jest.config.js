d6864be3cc933da30dab250e1228abc8a3eae414
João Carmo <kingjoao@gmail.com>
2021-11-07 12:35:31 +0000

    Includes the module in building

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,6 @@
 const esModules = ['escape-string-regexp'].join('|');
 
 module.exports = {
-  globals: {
-    'ts-jest': {
-      diagnostics: {
-        ignoreCodes: [
-          'TS7006', /* Parameter 'string' implicitly has an 'any' type. */
-        ],
-      },
-    },
-  },
   roots: ['<rootDir>/src/', '<rootDir>/test/'],
   transform: {
     '^.+\\.[tj]sx?$': 'ts-jest',
